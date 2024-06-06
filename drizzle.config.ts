0fa9d331adc0c00702fcfdfe7c41034bf00cce99
Idnk <efeidukpaye@gmail.com>
2024-06-06 20:20:22 +0100

    init

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["n3-gallery_*"],
+} satisfies Config;
