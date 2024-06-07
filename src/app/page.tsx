fe97bbdd81889dd66084c1906b236b6a0294d5bf
Idnk <efeidukpaye@gmail.com>
2024-06-07 14:28:36 +0100

    force-dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const imgUrlList = [
   "https://utfs.io/f/f86f7614-9086-436c-ae16-9dbde6e99c27-2i31a6.webp",
   " https://utfs.io/f/d67fef67-5341-4df9-8ac7-b46e895b2698-gva93o.png ",
