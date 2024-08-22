36878ac8bf3ae9b24dec13879fc7674a954c625a
Idnk <efeidukpaye@gmail.com>
2024-08-22 12:10:42 +0100

    modal clean up

--- src/components/PhotoPageContainer.tsx
+++ src/components/PhotoPageContainer.tsx
@@ -2,5 +2,18 @@ import { getImage } from "~/server/queries";
 
 export default async function PhotoPageContainer({ id }: { id: number }) {
   const Image = await getImage(id);
-  return <img src={Image.url} className="h-56 w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0 ">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          src={Image.url}
+          className="flex-shrink object-contain"
+          alt={"The is the image of a car"}
+        />
+      </div>
+      <div className="w-48 flex-shrink-0 border-l">
+        <p className="text-xl font-bold">{Image.name}</p>
+      </div>
+    </div>
+  );
 }
